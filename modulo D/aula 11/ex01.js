c0835fdd099193c6a75c95fa8899233f142d1ddc
diogo varaschin <diogovaraschin@outlook.com>
2025-01-14 17:30:33 -0300

    modulo D, aula 11

---
+++ modulo D/aula 11/ex01.js
@@ -0,0 +1,10 @@
+//console.log ('ta funcionando') 
+var vel = 55
+console.log (`a velocidade do seu carro eh ${vel}km/h`)
+if (vel > 60) {
+    console.log (`ultrapassou a velocidade permitida. MULTADO!`)
+}
+else {
+    console.log (`siga o seu caminho, boa viagem`)
+}
+console.log (`dirija sempre usando o cinto`)
---
+++ modulo D/aula 11/ex02.js
@@ -0,0 +1,8 @@
+var pais = 'brasil'
+console.log (`vivendo em ${pais}`)
+if (pais=='brasil') {
+    console.log (`voce eh brasileiro`)
+}
+else {
+    console.log (`voce eh estrangeiro`)
+}
