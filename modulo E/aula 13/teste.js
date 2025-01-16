321b3101bd512794388d85eaefafe670ce62b599
diogo varaschin <diogovaraschin@outlook.com>
2025-01-16 10:59:32 -0300

    modulo E, aula 13

---
+++ modulo E/aula 13/teste.js
@@ -0,0 +1,17 @@
+/* console.log ('lionel messi')
+console.log ('lionel messi')
+console.log ('lionel messi')
+console.log ('lionel messi')
+console.log ('lionel messi') */
+
+/* var messi = 1
+while (messi <= 7) {
+    console.log (`lionel messi ${messi}`)
+   messi++
+} */
+
+var messi = 1
+do {
+    console.log (`lionel messi ${messi}`)
+    messi++
+} while (messi <= 10)
