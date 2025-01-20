7036b7fa6f351c62cc245d4eeb9e6fc8a2bad9fe
diogo varaschin <diogovaraschin@outlook.com>
2025-01-20 16:33:48 -0300

    finalizacao do exercicio da aula 16

--- JS básico - gustavo guanabara/modulo F/aula 16 exer pt.1/script.js
+++ JS básico - gustavo guanabara/modulo F/aula 16 exer pt.1/script.js
@@ -21,8 +21,40 @@ function inLista(n, l){
 
 function add() {
     if (isNum(num.value) && !inLista(num.value, valores)){
-        window.alert('tudo ok')
+        valores.push(Number(num.value))
+        var item=document.createElement('option')
+        item.text=`valor ${num.value} foi adicionado`
+        lista.appendChild(item)
+        res.innerHTML=''
     } else {
     window.alert('valor invalido ou ja encontrado na lista')
+    }
+    num.value=''
+    num.focus()
+}
+
+function end() {
+    if (valores.length==0){
+        window.alert('adicione valores antes de finalizar')
+    } else {
+        var total = valores.length
+        var maior = valores[0]
+        var menor = valores[0]
+        var soma = 0
+        var media = 0
+        for (var pos in valores){
+            soma+=valores[pos]
+            if (valores[pos]<menor)
+                menor=valores[pos]
+            if (valores[pos]>maior)
+                maior=valores[pos]
+        }
+        media=soma/total
+        res.innerHTML=''
+        res.innerHTML+=`<p>ao todo temos ${total} numeros</p>`
+        res.innerHTML+=`<p>o menor valor foi ${menor}</p>`
+        res.innerHTML+=`<p>o maior valor foi ${maior}</p>`
+        res.innerHTML+=`<p>a soma de todos os valores eh: ${soma}</p>`
+        res.innerHTML+=`<p>a media dos valores eh: ${media}</p>`
     }
 }
