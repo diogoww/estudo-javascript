e97cce4c1db0d8d09eda0b2b41553c1891e55e71
diogo varaschin <diogovaraschin@outlook.com>
2025-01-22 18:00:27 -0300

    audio AND keycode

--- 2 - manipulação de DOM/manipulação DOM (PIANO) - mayk brito/main.js
+++ 2 - manipulação de DOM/manipulação DOM (PIANO) - mayk brito/main.js
@@ -2,7 +2,25 @@
 const keys = document.querySelectorAll(".key")
 
 // play notes
-function playNote(event)
+function playNote(event) {
+    let audioKeyCode = getKeyCode(event)  
+    //typed or pressed key
+    //if key exists
+    //play audio
+}
+
+function getKeyCode (event) {
+    let keyCode
+    const isKeyboard = event.type === "keydown" 
+    if (isKeyboard) {
+        keyCode = event.keyCode
+    } else {
+        keyCode = event.target.dataset.key
+    }
+}
 // click with mouse
+keys.forEach(function(key) {
+    key.addEventListener("click", playNote)
+})
 // keyboard type
 window.addEventListener("keydown", playNote)
