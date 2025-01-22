886cba9b2064254162ee583d9d79616b298f1f31
diogo varaschin <diogovaraschin@outlook.com>
2025-01-22 17:41:24 -0300

    planejando como o piano vai funcionar

---
+++ 2 - manipulação de DOM/manipulação DOM (PIANO) - mayk brito/main.js
@@ -0,0 +1,8 @@
+// get all keys
+const keys = document.querySelectorAll(".key")
+
+// play notes
+function playNote(event)
+// click with mouse
+// keyboard type
+window.addEventListener("keydown", playNote)
