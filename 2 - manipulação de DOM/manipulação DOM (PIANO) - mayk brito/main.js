2d5ff84d51c33dd6f1677f0cde29178c2a910299
diogo varaschin <diogovaraschin@outlook.com>
2025-01-22 18:27:06 -0300

    piano finalizado xD

--- 2 - manipulação de DOM/manipulação DOM (PIANO) - mayk brito/main.js
+++ 2 - manipulação de DOM/manipulação DOM (PIANO) - mayk brito/main.js
@@ -1,12 +1,18 @@
-// get all keys
 const keys = document.querySelectorAll(".key")
 
-// play notes
 function playNote(event) {
     let audioKeyCode = getKeyCode(event)  
-    //typed or pressed key
-    //if key exists
-    //play audio
+    const key = document.querySelector(`.key[data-key="${audioKeyCode}"]`)
+    const cantFoundAnyKey = key
+    if (!cantFoundAnyKey) {
+        return
+    }
+    playAudio(audioKeyCode)
+    addPlayingClass(key)
+}
+
+function addPlayingClass (key) {
+    key.classlist.add('playing')
 }
 
 function getKeyCode (event) {
@@ -17,10 +23,27 @@ function getKeyCode (event) {
     } else {
         keyCode = event.target.dataset.key
     }
+    return keyCode
 }
-// click with mouse
-keys.forEach(function(key) {
-    key.addEventListener("click", playNote)
-})
-// keyboard type
-window.addEventListener("keydown", playNote)
+
+function playAudio(audioKeyCode) {
+    const audio = document.querySelector(`audio[data-key="${audioKeyCode}"]`)
+    audio.currentTime = 0
+    audio.play() 
+}
+
+function removePlayingClass(event) {
+    event.target.classList.remove("playing")
+}
+
+function registerEvents() {
+
+    keys.forEach(function(key) {
+        key.addEventListener("click", playNote)
+        key.addEventListener("transitionend", removePlayingClass)
+    })
+    
+    window.addEventListener("keydown", playNote)
+}
+
+window.addEventListener("load", registerEvents)
