8e1a96cb02f52117123a4e7ed6377c44a16df6d1
diogo varaschin <diogovaraschin@outlook.com>
2025-01-23 20:33:23 -0300

    projeto finalizado

--- 3 -  fetch API - ajax (XHR)/fetch api (com projeto) - matheus battisti (hora de codar)/js/script.js
+++ 3 -  fetch API - ajax (XHR)/fetch api (com projeto) - matheus battisti (hora de codar)/js/script.js
@@ -3,6 +3,17 @@ const url = "https://jsonplaceholder.typicode.com/posts"
 const loadingElement = document.querySelector("#loading")
 const postsContainer = document.querySelector("#posts-container")
 
+const postPage = document.querySelector("#post")
+const postContainer = document.querySelector("#post-container")
+const commentsContainer = document.querySelector("#comments-container")
+
+const commentForm = document.querySelector("#comment-form")
+const emailInput = document.querySelector("#email")
+const bodyInput = document.querySelector("#body")
+
+const urlSearchParams = new URLSearchParams(window.location.search)
+const postId = urlSearchParams.get("id")
+
 async function getAllPosts() {
     const response = await fetch(url)
     console.log(response)
@@ -30,4 +41,72 @@ async function getAllPosts() {
     })
 }
 
-getAllPosts()
+async function getPost(id) {
+    const [responsePost, responseComments] = await Promise.all ([
+        fetch (`${url}/${id}`),
+        fetch (`${url}/${id}/comments`)
+    ])
+
+    const dataPost = await responsePost.json()
+    const dataComments = await responseComments.json()
+    loadingElement.classList.add("hide")
+    postPage.classList.remove("hide")
+
+    const title = document.createElement("h1")
+    const body = document.createElement("p")
+    title.innerText = dataPost.title
+    body.innerText = dataPost.body
+
+    postContainer.appendChild(title)
+    postContainer.appendChild(body)
+
+    dataComments.map((comment) => {
+        createComment(comment)
+    })
+    
+}
+
+function createComment(comment) {
+    const div = document.createElement("div")
+    const email = document.createElement("h3")
+    const commentBody = document.createElement("p")
+
+    email.innerText = comment.email
+    commentBody.innerText = comment.body
+
+    div.appendChild(email)
+    div.appendChild(commentBody)
+
+    commentsContainer.appendChild(div)
+}
+
+// post a comment
+async function postComment(comment) {
+    const response = await fetch(`${url}/${postId}/comments`, {
+        method: "POST",
+        body: comment,
+        headers: {
+            "Content-type": "application/json",
+        },
+    })
+    const data = await response.json()
+    createComment(data)
+}
+
+if (!postId){
+    getAllPosts()
+} else {
+    getPost(postId)
+    
+    // add event to comment form
+    commentForm.addEventListener("submit", (e) => {
+        e.preventDefault()
+
+        let comment = {
+            email: emailInput.value,
+            body: bodyInput.value
+        }
+        comment = JSON.stringify(comment)
+        postComment(comment)
+    })
+}
