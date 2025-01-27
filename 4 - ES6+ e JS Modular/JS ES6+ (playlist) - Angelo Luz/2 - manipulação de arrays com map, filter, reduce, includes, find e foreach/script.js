1b2eeb9b9740fa9d11993d4d567374af0133f817
diogo varaschin <diogovaraschin@outlook.com>
2025-01-27 12:09:29 -0300

    aula 2 concluida

--- 4 - ES6+ e JS Modular/JS ES6+ (playlist) - Angelo Luz/2 - manipulação de arrays com map, filter, reduce, includes, find e foreach/script.js
+++ 4 - ES6+ e JS Modular/JS ES6+ (playlist) - Angelo Luz/2 - manipulação de arrays com map, filter, reduce, includes, find e foreach/script.js
@@ -1,12 +1,12 @@
-const clients = [
+let clients = [
     {name: 'diogo varaschin', birth: 2003, genre: 'M', purchaseDaysAgo: 3},
-    {name: 'messi', birth: 1987, genre: 'M', purchaseDaysAgo: 5},
-    {name: 'jinx', birth: 2008, genre: 'F', purchaseDaysAgo: 1},
+    {name: 'lionel messi', birth: 1987, genre: 'M', purchaseDaysAgo: 5},
+    {name: 'jinx powder', birth: 2008, genre: 'F', purchaseDaysAgo: 1},
     {name: 'hellen martins', birth: 1994, genre: 'F', purchaseDaysAgo: 25},
     {name: 'ronaldinho gaucho', birth: 1980, genre: 'M', purchaseDaysAgo: 14}
 ]
 
-let ages = [18, 22, 45, 36, 5, 99, 56, 30, 22, 51]
+let ages = [18, 22, 45, 36, 5, 99, 56, 30, 22, 51, 1, 6, 2, 9]
 console.log(ages)
 
 // altera todos o valores em +1
@@ -18,3 +18,38 @@ const impar = ages.filter(value => (value%2 === 1))
 console.log(impar)
 
 // altera todos os valores imapres para o proximo par
+ages = ages.map(value => (value%2===1? value+1 : value));
+console.log(ages)
+
+// cria um novo array com os valores menores do que 20 
+const menores = ages.filter(value => value < 20)
+console.log(menores)
+
+// cria um novo array com os valores menores do que 17 e incrementar +1
+const menores2 = ages
+    .filter(value => value < 17)
+    .map(value => value+1)
+console.log(menores2)
+
+// retorne a soma de todos os valores menores do que 20
+const somaMenores20 = ages
+    .filter(value => value < 20)
+    .reduce((acc, next) => (acc += next))
+console.log(somaMenores20)
+
+console.log(clients)
+// incrementa em 1 o purschaseDaysAgo de todos os objetos
+let newClients = clients.map(v =>({...v}))
+newClients = newClients.map(value => {
+    value.purchaseDaysAgo+=1
+    return value
+})
+console.log(newClients)
+
+// verificar se um determinado numero de entrada esta contido no array
+console.log(ages.includes(20))
+console.log(ages.find(value => value===20))
+
+// altera array para ficar so com o primeiro nome dos clientes
+let nameClients = clients.map(value => value.name.split(" ")[0])
+console.log(nameClientslients)
