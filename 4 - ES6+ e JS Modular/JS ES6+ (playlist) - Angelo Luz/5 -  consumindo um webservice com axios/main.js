553880927c183dcd832b1fd441eac69a1f9490e3
diogo varaschin <diogovaraschin@outlook.com>
2025-01-29 12:24:46 -0300

    aula 5 concluida

--- 4 - ES6+ e JS Modular/JS ES6+ (playlist) - Angelo Luz/5 -  consumindo um webservice com axios/main.js
+++ 4 - ES6+ e JS Modular/JS ES6+ (playlist) - Angelo Luz/5 -  consumindo um webservice com axios/main.js
@@ -1,5 +1,28 @@
 import axios from 'axios'
 
-class Api{
-    static getAddres
+// primeira maneira: 
+/* class Api{
+    static async getAddres (cep){
+        const response = await (await (axios.get(`https://viacep.com.br/ws/${cep}/json/`))).data
+        console.log(response)
+    }
 }
+Api.getAddres('86420000') */
+
+// segunda maneira:
+class Api{
+    static async getAddress (cep){
+        const response = new Address((await (axios.get(`https://viacep.com.br/ws/${cep}/json/`))).data)
+        return response
+    }
+}
+
+class Address{
+    constructor({cep, localidade, estado}){
+        this.cep = cep
+        this.localidade = localidade
+        this.estado = estado
+    }
+}
+
+Api.getAddress('86420000').then(v => {console.log(v)})
