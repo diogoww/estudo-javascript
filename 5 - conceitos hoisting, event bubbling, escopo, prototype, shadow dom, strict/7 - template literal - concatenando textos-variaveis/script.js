e72e4c2ce9281613866fe89ead9ba49d3daa6aba
diogo varaschin <diogovaraschin@outlook.com>
2025-01-30 14:22:24 -0300

    7 - template literal

---
+++ 5 - conceitos hoisting, event bubbling, escopo, prototype, shadow dom, strict/7 - template literal - concatenando textos-variaveis/script.js
@@ -0,0 +1,18 @@
+const brasil = {
+    live: 'carlópolis',
+    love: 'curitiba'
+}
+
+const text = 'eu moro em ' + brasil.live + ' e sou apaixonado por ' + brasil.love + '!'
+const newText = `eu moro em ${brasil.live} e sou apaixonado por ${brasil.love}`
+
+//console.log(text)
+//console.log(newText)
+
+const frutas = 'banana'
+                + '\n'
+                + 'laranja'
+                + '\n'
+                + 'maça'
+
+console.log(frutas)
