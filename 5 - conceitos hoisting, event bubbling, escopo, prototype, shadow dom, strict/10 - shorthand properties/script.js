57cc7ec2fda3f3266f55a814a592e90dfe3bf98c
diogo varaschin <diogovaraschin@outlook.com>
2025-01-31 18:33:01 -0300

    shorthand properties

---
+++ 5 - conceitos hoisting, event bubbling, escopo, prototype, shadow dom, strict/10 - shorthand properties/script.js
@@ -0,0 +1,16 @@
+let firstName = 'Diogo'
+let surname = 'Varaschin'
+let age = '21'
+
+const person = {
+    firstName,
+    surname,
+    age,
+
+    hello(){
+        console.log('olá')
+    }
+}
+
+console.log(person)
+person.hello()
