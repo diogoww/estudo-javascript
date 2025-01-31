3e452bf4905246a09dcbf39f5d6cd31556c91b69
diogo varaschin <diogovaraschin@outlook.com>
2025-01-31 18:51:12 -0300

    12 - metódos strings - startsWith, endsWith, includes

---
+++ 5 - conceitos hoisting, event bubbling, escopo, prototype, shadow dom, strict/12 - metodos strings/script.js
@@ -0,0 +1,6 @@
+let text = 'ola lionel messi, o meu nome é diogo varaschin'
+
+console.log(text.startsWith('la', 1))
+console.log(text.endsWith('varasch', 44))
+console.log(text.includes('messi')) // procura algo na string
+console.log('jinx '.repeat(5))
